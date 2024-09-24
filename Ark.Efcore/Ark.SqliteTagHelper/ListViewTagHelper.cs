6578132c05488d78e02418fbbea5b2d711e1c0d2
Immanuel Raj <kimr@solar.dk>
2024-09-24 08:29:51 +0530

    addl cahnges.

---
+++ Ark.Efcore/Ark.SqliteTagHelper/ListViewTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.ComponentModel;
+
+namespace Ark.View
+{
+    [HtmlTargetElement("list-view")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class ListViewTagHelper : TagHelper
+    {
+        //public override void Process(TagHelperContext context, TagHelperOutput output)
+        //{
+        //    if (string.Equals(context.TagName, "list-view",
+        //                      StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        output.TagName = "table";
+        //        output.Attributes.Add("style", "border-radius: 16px 16px 0px 0px;width: 100%;border-collapse: collapse;");
+        //    }
+        //}
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            base.ProcessAsync(context, output);
+        }
+    }
+}
